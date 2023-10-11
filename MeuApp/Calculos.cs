04c56953bb7388db238977a9d425bd8ecc840085
User <User@DESKTOP-BO2BA22>
2023-10-11 20:31:33 -0300

    Adicionar arquivos de projeto.

---
+++ MeuApp/Calculos.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Equacoes
+{
+    public class Heren
+    {
+        public double A, B, C;
+        public double SomaHeren()
+        {
+            double p = (A + B + C) / 2;
+            Console.WriteLine("Calculo achar o p: ");
+            Console.WriteLine("p = (A+B+C) / 2");
+            Console.WriteLine("p = ({0} + {1} + {2}) / 2", A, B, C);
+            Console.WriteLine("p = {0} / 2", A + B + C);
+            Console.WriteLine("p = {0}", p);
+            Console.WriteLine(" ");
+            Console.WriteLine("Calculo da formula Heren: ");
+            Console.WriteLine("v {0} * ({1} - {2}) * ({3} - {4}) * ({5} - {6})", p, p, A, p, B, p, C);
+            Console.WriteLine("v {0} * {1} * {2} * {3}", p, p - A, p - B, p - C);
+            Console.WriteLine("v {0} * {1}", p * (p - A), (p - B) * (p - C));
+            Console.WriteLine("v {0}", p * (p - A) * (p - B) * (p - C));
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
+
+    }
+    public class Bhaskara
+    {
+        public double A, B, C;
+        public string SomaBhaskara()
+        {
+            double Delta = B * B - 4 * A * C;
+            Console.WriteLine("d = {0} * {1} - 4 * {2} * {3}", B, B, A, C);
+            Console.WriteLine("d = {0} - 4 * {1}", B * B, A * C);
+            if (-4 * (A * C) < 0)
+                Console.WriteLine("d = {0}{1}", B * B, -4 * (A * C));
+            else
+            {
+                Console.WriteLine("d = {0} + {1}", B * B, -4 * (A * C));
+            }
+            Console.WriteLine("d = {0}", B * B - 4 * A * C);
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Calculo de Bhaskara: ");
+            Console.WriteLine("x = -b +- raiz de delta / 2 * a");
+            Console.WriteLine("x = {0} + v{1} / 2 * A", -B, Delta, A);
+            Console.WriteLine(" ");
+
+            Console.WriteLine("x1 = {0} + {1} / 2 * {2}", -B, Math.Sqrt(Delta), A);
+            Console.WriteLine("x1 = {0} / {1}", -B + Math.Sqrt(Delta), 2 * A);
+            Console.WriteLine("x1 = {0}", (-B + Math.Sqrt(Delta)) / (2 * A));
+            Console.WriteLine(" ");
+            Console.WriteLine("x2 = {0} - {1} / 2 * {2}", -B, Math.Sqrt(Delta), A);
+            Console.WriteLine("x2 = {0} / {1}", -B - Math.Sqrt(Delta), 2 * A);
+            Console.WriteLine("x2 = {0}", (-B - Math.Sqrt(Delta)) / (2 * A));
+            double x1, x2;
+            x1 = (-B + Math.Sqrt(Delta)) / (2 * A);
+            x2 = (-B - Math.Sqrt(Delta)) / (2 * A);
+
+            string result = string.Format($"As raizes da equação {A}*2 {B}x {C} = 0 são {x1} e {x2}");
+            return result;
+        }
+    }
+
+}
---
+++ MeuApp/Inicio.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Main
+{
+    class Inicio
+    {
+        static void Main(string[] args)
+        {
+            inicio:
+            Console.WriteLine("----------------------");
+            Console.WriteLine("-  CALCULADORA AUTO  -");
+            Console.WriteLine("----------------------");
+            Console.WriteLine("[1] Formula de Heren");
+            Console.WriteLine("[2] Formula de Bhaskara");
+
+            sbyte Selecionar = sbyte.Parse(Console.ReadLine());
+
+            switch (Selecionar)
+            {
+                case 1:
+                {
+                    Equacoes.Heren X = new Equacoes.Heren();
+                    Console.WriteLine("Digite o valor de A: ");
+                    X.A = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Digite o valor de B: ");
+                    X.B = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Digite o valor de C: ");
+                    X.C = double.Parse(Console.ReadLine());
+
+                    double AreaX = X.SomaHeren();
+                    Console.WriteLine("Valor da area: {0:F2}", AreaX);
+
+                    break;
+                }
+                case 2:
+                {
+                    Equacoes.Bhaskara X = new Equacoes.Bhaskara();
+                    Console.WriteLine("Digite o valor de A: ");
+                    X.A = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Digite o valor de B: ");
+                    X.B = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Digite o valor de C: ");
+                    X.C = double.Parse(Console.ReadLine());
+
+                    string Result = X.SomaBhaskara();
+                    Console.WriteLine(Result);
+                    break;
+                }
+                default:
+                    break;
+            }
+            Console.WriteLine("");
+            Console.WriteLine("[1] Fazer outro calculo");
+            Console.WriteLine("[2] Sair");
+            Selecionar = sbyte.Parse(Console.ReadLine());
+            switch (Selecionar)
+            {
+                case 1:
+                {
+                    Console.Clear();
+                    goto inicio;
+                    break;
+                }
+                case 2:
+                {
+                    Console.WriteLine("Voce saiu do programa.");
+                    break;
+                }
+                default:
+                {
+                    Console.WriteLine("Voce saiu do programa.");
+                    break;
+                }
+            }
+        }
+    }
+
+}
